2438661a983618234174816d24f21391cb8c5b19
unaidelao <unai.delao@gmail.com>
2017-04-02 11:22:21 +0200

    Add wherefore-art-thou.js

---
+++ Algorithms/wherefore-art-thou.js
@@ -0,0 +1,35 @@
+/*
+ * Make a function that looks through an array of objects (first argument) and
+ * returns an array of all objects that have matching property and value pairs
+ * (second argument). Each property and value pair of the source object has to
+ * be present in the object from the collection if it is to be included in the
+ * returned array.
+ *
+ * For example, if the first argument is [{ first: "Romeo", last: "Montague" },
+ * { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }],
+ * and the second argument is { last: "Capulet" }, then you must return the
+ * third object from the array (the first argument), because it contains the
+ * property and its value, that was passed on as the second argument.
+ *
+ * Help: Global Object
+ *       Object.prototype.hasOwnProperty()
+ *       Object.keys()
+ */
+
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+  var keys = Object.keys(source);
+
+  arr = collection.filter(function(x) {
+    return keys.every(function(key){
+      return x.hasOwnProperty(key) && x[key] === source[key];
+    });
+  });
+
+  // Only change code above this line
+  return arr;
+}
+
+console.log(whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 }));

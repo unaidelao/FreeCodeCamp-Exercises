b2ca13effda568a8ef85268dfaa22ed217034167
unaidelao <unai.delao@gmail.com>
2017-09-15 18:18:44 +0200

    Add boo-whoo.js

---
+++ Algorithms/boo-whoo.js
@@ -0,0 +1,12 @@
+/*
+ * Boo-Whoo
+ *
+ * Check if a value is classified as a boolean primitive. Return true or false.
+ * Boolean primitives are true and false.
+ */
+ 
+ function booWho(bool) {
+  return typeof bool === 'boolean';
+ }
+
+ console.log(booWho(1));

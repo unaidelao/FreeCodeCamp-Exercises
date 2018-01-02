2b25ec568f5b3115ddb93e52cfe3c14a4b90d7a7
unaidelao <unai.delao@gmail.com>
2018-01-02 02:08:50 +0100

    Add drop-it.js

---
+++ Algorithms/drop-it.js
@@ -0,0 +1,25 @@
+/*
+ * Drop It Algorithm
+ *
+ * Drop the elements of an array (first argument), starting from the front,
+ * until the predicate (second argument) returns true.
+ *
+ * The second argument, func, is a function you'll use to test the first
+ * elements of the array to decide if you should drop it or not.
+ *
+ * Return the rest of the array, otherwise return an empty array.
+ */
+ 
+ function dropElements(arr, func) {
+   
+   var totalLength = arr.length;
+   for (var i = 0; i < arr.totalLength; i++) {
+     if (func(arr[0])) {
+       break;
+     }
+     else {
+       arr.shift();
+     }
+   }
+   return arr;
+ }

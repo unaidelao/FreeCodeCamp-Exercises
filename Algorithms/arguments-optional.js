233da0c94c8d735981a012d99747606a26aad6d0
unaidelao <unai.delao@gmail.com>
2018-01-03 11:46:11 +0100

    Add arguments-optional.js

---
+++ Algorithms/arguments-optional.js
@@ -0,0 +1,31 @@
+/*
+ * Arguments Optional Algorithm
+ *
+ * Create a function that sums two arguments together. If only one argument is
+ * provided, then return a function that expects one argument and returns the
+ * sum.
+ *
+ * For example, addTogether(2, 3) should return 5, and addTogether(2) should
+ * return a function.
+ *
+ * Calling this returned function with a single argument will then return the
+ * sum:
+ *
+ * var sumTwoAnd = addTogether(2);
+ *
+ * sumTwoAnd(3) returns 5.
+ *
+ * If either argument isn't a valid number, return undefined.
+ */
+ 
+ function addTogether(firstArg, secondArg) {
+   
+  if (arguments.length === 1 && typeof firstArg === 'number') {
+    return function (secondArg) {
+      return addTogether (firstArg, secondArg);
+    };
+  }
+  else if (typeof secondArg === 'number') {
+    return firstArg + secondArg;
+  }
+}

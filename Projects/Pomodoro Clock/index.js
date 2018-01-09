d2c7272d009a2ad536e96035303addbf24c2b8fd
unaidelao <unai.delao@gmail.com>
2018-01-09 11:09:42 +0100

    modified index.js

--- Projects/Pomodoro Clock/index.js
+++ Projects/Pomodoro Clock/index.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-  
+  // Declaración de variables.
+  var audio = $("audio")[0];
+  var pause = false;
+  var sessionTime = 25;
+  var breakTime = 5;
+  var minutes = 25;
+  var seconds = 0;
+  var timeCounter;
+
+  // Le pone secundero al reloj.
+  $(".clockTimer").html(minutes + ":00");
 });
